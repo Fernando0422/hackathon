4cbb87ce3011ea663cb7d003db06305aeb42c823
Kara Barrese <karabarrese@gmail.com>
2022-11-13 01:41:25 -0800

    appdate
    it's an update.... for app

--- app.js
+++ app.js
@@ -9,30 +9,63 @@ app.set('json spaces', 2);
 
 app.get('/gettoken', async function (req, res) {
 
-    console.log(req);
+    //console.log(req);
     //res.send("res")
 
     //Set up URL to query
-    let appId = "2qea8q65if";
-    let hashToken = "MnFlYThxNjVpZnx3RWNHSmtYMmJVN0VJTERhalJMME8xWVRpaml4SmlrNWFhNE5RNmN4";
-    let url = `https://api.iq.inrix.com/auth/v1/appToken?appId=${appId}&hashToken=${hashToken}`;
+    let appId = "_QKWYagu9qv1PjXLTjtc9Q";
+    let hashToken = "m_rvsvDjWNKJY-6p05B_oeDurrY0SPbtXCdvSyoyS6wBgjNw7A2MZ06x3rFEp-8ukkoONVi_gnhbk4spc84MHjooIcWfx2-erBt4uGno3gqaYUH8ZMZr0S5wGLFwY3Yx";
+    let url = `https://api.yelp.com/v3/businesses/search?latitude=37.783926&longitude=-122.433072&radius=8046&open_now=true`;
 
     //Set up query method
     var requestOptions = {
         method: 'GET' ,
-        headers: {Authorization: 'Bearer' + hashToken},
+        headers: { Authorization: 'Bearer ' + hashToken},
         redirect: 'follow'
+        //how to define custom functions in node js - how to export function and imoport it on the other file
     };
 
     //Query INRIX for token
     let response = await fetch(url, requestOptions);
     let json = await response.json();
-    let output = json.result.token;
+   // let output = json.result.token;
 
     //Return token
-    res.json({
-        token: output,
-    });
+    // res.json({
+    //     token: output,
+    // });
+
+    var url2 = "https://api.yelp.com/v3/businesses/search?latitude=37.783926&longitude=-122.433072&radius=8046&open_now=true" 
+
+    //Set up query method
+    var requestOptions2 = {
+    method: 'GET',
+    headers: { Authorization: 'Bearer ' + hashToken},
+    redirect: 'follow'
+    };
+
+    //Make query using fetch
+    var output2 = fetch(url2, requestOptions2)
+    .then(response => response.json())
+    .then(result => handleOutput(result))
+    .catch(error => console.log('error', error));
+
+    function handleOutput(value){
+        //var jsonList = [];
+        var jsonList = {
+            result: []
+        };
+        for (let i = 0; i < value.businesses.length; i++) {
+            jsonList.result.push({
+                "name" : value.businesses[i].name,
+                "longitude" : value.businesses[i].coordinates.longitude,
+                "latitude" : value.businesses[i].coordinates.latitude
+            });
+        }
+        res.json({
+            jsonList,
+        });
+      }
 })
 
 //app.listen(8000, ()=>console.log("proxy has beem started at port 8000"));
